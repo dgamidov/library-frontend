e3ac6b612e652dbc402fa5330a8e07f6290f7cd6
Denis Gamidov <dgamidov@gmail.com>
2022-08-12 19:01:46 +0300

    Add data d3 tree based on fetched data

--- src/views/CSBooksView.vue
+++ src/views/CSBooksView.vue
@@ -2,14 +2,14 @@
   <v-container id="viewCSBooks">
     <div class="text-h2">Книги по CS</div>
 
-    <div v-if="listOfCSBooks" class="content">
-      <h2>{{ listOfCSBooks }}</h2>
-    </div>
+    <div id="tree" class="text-center"></div>
   </v-container>
 </template>
 
 <script>
-  const yaml = require('js-yaml');
+  const yaml = require('js-yaml')
+  import Tree from '../plugins/d3/tree-radial-cluster.js'
+  import * as d3 from 'd3'
 
   export default {
     name: 'CS Books',
@@ -26,7 +26,7 @@
       this.$watch(
         () => this.$route.params,
         () => {
-          this.fetchData()
+          this.fetchDataAndDrawTree()
         },
         // fetch the data when the view is created and the data is
         // already being observed
@@ -34,13 +34,27 @@
       )
     },
     methods: {
-      fetchData() {
+      fetchDataAndDrawTree() {
         this.error = this.listOfCSBooks = null
         this.loading = true
 
         fetch('/data/cs-books.yaml').then((response) => {
           return response.text().then((data) => {
-            this.listOfCSBooks = yaml.load(data)
+
+            const listOfCSBooks = yaml.load(data)
+            const chart = Tree(listOfCSBooks, {
+              tree: d3.cluster,
+              label: d => d.name,
+              title: d => `[${d.status}] ${d.name}${d.author ? ' - ' + d.author : ''}`, // hover text
+              link: d => d.link,
+              width: 1400,
+              height: 1400,
+              margin: 300,
+              stroke: '#0040ff'
+            });
+
+            const div = document.getElementById('tree');
+            div.appendChild(chart);
           }).catch((err) => {
             this.error = err.toString()
           })
