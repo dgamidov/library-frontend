850a0de599b9b6167f6fc5fe62d61f3ff9fa9d30
Denis Gamidov <dgamidov@gmail.com>
2022-08-12 18:09:01 +0300

    Improve header design

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,20 @@
 <template>
-  <v-card>
-    <v-tabs
-      background-color="deep-purple accent-4"
-      center-active
-      dark
-    >
-      <v-tab to="/">Главная</v-tab>
-      <v-tab to="/cs-books">Книги по CS</v-tab>
-    </v-tabs>
-  </v-card>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-card>
+          <v-tabs
+            background-color="deep-purple accent-4"
+            center-active
+            dark
+          >
+            <v-tab to="/">Главная</v-tab>
+            <v-tab to="/cs-books">Книги по CS</v-tab>
+          </v-tabs>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
