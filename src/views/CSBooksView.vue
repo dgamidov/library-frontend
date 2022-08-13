e261d7e50216194c1dd0c21c36666994fcca1988
Denis Gamidov <dgamidov@gmail.com>
2022-08-13 12:11:34 +0300

    Refactor drawing tree

--- src/views/CSBooksView.vue
+++ src/views/CSBooksView.vue
@@ -2,13 +2,14 @@
   <v-container id="viewCSBooks">
     <div class="text-h2">Книги по CS</div>
 
-    <div id="tree" class="text-center"></div>
+    <div class="text-center">
+      <svg></svg>
+    </div>
   </v-container>
 </template>
 
 <script>
   const yaml = require('js-yaml')
-  import Tree from '../plugins/d3/tree-radial-cluster.js'
   import * as d3 from 'd3'
 
   export default {
@@ -17,53 +18,127 @@
     data() {
       return {
           tab: null,
-          listOfCSBooks: null,
           error: null,
-          tree_exists: false
+          books: null,
         }
       },
-    created() {
-      // watch the params of the route to fetch the data again
-      this.$watch(
-        () => this.$route.params,
-        () => {
-          this.fetchDataAndDrawTree()
-        },
-        // fetch the data when the view is created and the data is
-        // already being observed
-        { immediate: true }
-      )
+    mounted() {
+      this.fetchDataAndDrawTree();
     },
     methods: {
-      fetchDataAndDrawTree() {
-        this.error = this.listOfCSBooks = null
-        this.loading = true
-
-        if (!this.tree_exists) {
-          fetch('/data/cs-books.yaml').then((response) => {
-            return response.text().then((data) => {
-
-              const listOfCSBooks = yaml.load(data)
-              const chart = Tree(listOfCSBooks, {
-                tree: d3.cluster,
-                label: d => d.name,
-                title: d => `[${d.status}] ${d.name}${d.author ? ' - ' + d.author : ''}`, // hover text
-                link: d => d.link,
-                width: 1400,
-                height: 1400,
-                margin: 300,
-                stroke: '#0040ff'
-              });
-
-              const div = document.getElementById('tree');
-              div.appendChild(chart);
-
-              this.tree_exists = true
-            }).catch((err) => {
-              this.error = err.toString()
-            })
-          })
+      getNodeColor(status) {
+        // https://encycolorpedia.ru/ffbf00s
+        if (status == 'прочитал') {
+          return '#10e030';
+        } else if (status == 'читаю') {
+          return '#ff00ff';
+        } else if (status == 'купил') {
+          return '#ffd700';
+        } else if (status == 'перечитать') {
+          return '#00d8ff';
+        } else {
+          return '#555';
         }
+      },
+      fetchDataAndDrawTree() {
+        this.error = this.books = null;
+        // this.loading = true
+
+        fetch('/data/cs-books.yaml').then((response) => {
+          return response.text().then((raw_data) => {
+
+            this.books = yaml.load(raw_data);
+
+            const tree = d3.cluster;
+            const label = d => d.name;
+            const title = d => `[${d.status}] ${d.name}${d.author ? ' - ' + d.author : ''}`;
+            const link = d => d.link;
+            const stroke = '#0092f0';
+            const width = 1400;
+            const height = 1400;
+            const margin = 300;
+
+            const sort = null;
+            const strokeOpacity = null;
+            const strokeLinecap = null;
+            const strokeLinejoin = null;
+            const strokeWidth = null;
+
+            const separation = tree === d3.tree ? (a, b) => (a.parent == b.parent ? 1 : 2) / a.depth : (a, b) => a.parent == b.parent ? 1 : 2;
+            const linkTarget = "_blank";
+            const marginTop = margin; // top margin, in pixels
+            const marginRight = margin; // right margin, in pixels
+            const marginBottom = margin; // bottom margin, in pixels
+            const marginLeft = margin; // left margin, in pixels
+            const radius = Math.min(width - marginLeft - marginRight, height - marginTop - marginBottom) / 2; // outer radius
+            const r = 3; // radius of nodes
+            const halo = "#fff"; // color of label halo
+            const haloWidth = 3; // padding around the labels
+
+            const root = d3.hierarchy(this.books);
+
+            // Sort the nodes.
+            if (sort != null) root.sort(sort);
+
+            // Compute labels and titles.
+            const descendants = root.descendants();
+            const L = label == null ? null : descendants.map(d => label(d.data, d));
+
+            // Compute the layout.
+            tree().size([2 * Math.PI, radius]).separation(separation)(root);
+
+
+            const svg = d3.select("svg")
+                .attr("viewBox", [-marginLeft - radius, -marginTop - radius, width, height])
+                .attr("width", width)
+                .attr("height", height)
+                .attr("style", "max-width: 100%; height: auto; height: intrinsic;")
+                .attr("font-family", "sans-serif")
+                .attr("font-size", 10);
+
+            svg.append("g")
+                .attr("fill", "none")
+                .attr("stroke", stroke)
+                .attr("stroke-opacity", strokeOpacity)
+                .attr("stroke-linecap", strokeLinecap)
+                .attr("stroke-linejoin", strokeLinejoin)
+                .attr("stroke-width", strokeWidth)
+              .selectAll("path")
+              .data(root.links())
+              .join("path")
+                .attr("d", d3.linkRadial()
+                    .angle(d => d.x)
+                    .radius(d => d.y));
+
+            const node = svg.append("g")
+              .selectAll("a")
+              .data(root.descendants())
+              .join("a")
+                .attr("xlink:href", link == null ? null : d => link(d.data, d))
+                .attr("target", link == null ? null : linkTarget)
+                .attr("transform", d => `rotate(${d.x * 180 / Math.PI - 90}) translate(${d.y},0)`);
+
+            node.append("circle")
+                .attr("fill", d => d.children ? stroke : this.getNodeColor(d.data.status))
+                .attr("r", r);
+
+            if (title != null) node.append("title")
+                .text(d => title(d.data, d));
+
+            if (L) node.append("text")
+                .attr("transform", d => `rotate(${d.x >= Math.PI ? 180 : 0})`)
+                .attr("dy", "0.32em")
+                .attr("x", d => d.x < Math.PI === !d.children ? 6 : -6)
+                .attr("text-anchor", d => d.x < Math.PI === !d.children ? "start" : "end")
+                .attr("paint-order", "stroke")
+                .attr("stroke", halo)
+                .attr("stroke-width", haloWidth)
+                .text((d, i) => L[i]);
+
+          }).catch((err) => {
+            this.error = err.toString();
+          })
+        })
       }
     }
   }
